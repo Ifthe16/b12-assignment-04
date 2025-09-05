d7ef265ca331e9b2c46617f37e140a615bbaa8ee
Ifthe16 <habiburrahmanif975@gmail.com>
2025-09-05 16:50:29 +0600

    added problem-03.js file

---
+++ problem-03.js
@@ -0,0 +1,22 @@
+/* Problem-03 */
+function bestTeam(player1, player2) {
+    if (typeof player1 !== 'object' || typeof player2 !== 'object') {
+        return 'Invalid';
+    }
+    const fairPoint1 = player1.foul + player1.cardY + player1.cardR;
+    const fairPoint2 = player2.foul + player2.cardY + player2.cardR;
+    if (fairPoint1 > fairPoint2) {
+        return player2.name;
+    }
+    else if (fairPoint1 < fairPoint2) {
+        return player1.name;
+    }
+    else {
+        return "Tie";
+    }
+}
+
+const team1 = { name: "Brazil", foul: 5, cardY: 1, cardR: 0 };
+const team2 = { name: "Argentina", foul: 7, cardY: 0, cardR: 0 };
+const team = bestTeam(team1, team2);
+console.log(team);
