98faa7524793492ef04b7779263b5a47117c1040
Ifthe16 <habiburrahmanif975@gmail.com>
2025-09-06 16:00:47 +0600

    updated problem-02.js

--- problem-02.js
+++ problem-02.js
@@ -3,7 +3,7 @@ function onlyCharacter(str) {
     if (typeof str !== 'string') {
         return "Invalid";
     }
-    const character = str.split(' ').join('').toLocaleUpperCase();
+    const character = str.split(' ').join('').toUpperCase();
     return character;
 
 }
